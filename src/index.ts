7302abb59c38544b6827f28513d4eb1699531d46
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-12-05 14:03:05 +0100

    setup a back end

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express()
+
+app.use("/", (req, res) => {
+  console.log("Root route hit")
+
+  res.send("Henlo frens")
+})
+
+app.listen(3000, () => {
+  console.log("Server listen on port 3000")
+})
